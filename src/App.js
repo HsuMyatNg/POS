47b1983b174fb8fe79d5688afdff42e9712eeccd
HsuMyatNg <hsumyatnaing2626@gmail.com>
2021-11-20 22:12:52 +0630

    POS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import ShowProducts from "./Components/ShowProducts/ShowProducts";
+import CreateProduct from "./Components/CreateProduct/CreateProduct";
+import "./App.css";
+const App = () => {
+    const allProducts = [
+        {
+            id: "p1",
+            title: "Cola",
+            price: 2000,
+        },
+        {
+            id: "p2",
+            title: "Max+",
+            price: 1500,
+        },
+        {
+            id: "p3",
+            title: "Vito",
+            price: 1800,
+        },
+        {
+            id: "p4",
+            title: "Coke",
+            price: 2500,
+        },
+        {
+            id: "p5",
+            title: "Sponsor",
+            price: 600,
+        },
+        {
+            id: "p6",
+            title: "VeVe",
+            price: 1000,
+        },
+        {
+            id: "p7",
+            title: "Spy",
+            price: 3000,
+        },
+        {
+            id: "p8",
+            title: "Tea",
+            price: 400,
+        },
+    ];
+    return(
+        <div>
+            <div className="row">
+                <div className ="col-1 border-right p-0">
+                    <h1>First Part</h1>
+                </div>
+                <div className="col-2 border-right product_background_color">
+                    <ShowProducts products ={allProducts} />
+                </div>
+               <CreateProduct />
+            </div>
+        </div>
+    );
 }
-
 export default App;
---
+++ src/Components/CreateProduct/CreateProduct.js
@@ -0,0 +1,8 @@
+import ProductForm from "./ProductForm";
+import "./CreateProduct.css";
+const CreateProduct = (props) => {
+    return(
+        <div className="col-1"><ProductForm /></div>
+    );
+};
+export default CreateProduct;
